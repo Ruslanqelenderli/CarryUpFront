0bae1e01760bbb9993f442eea48fc5c049cdf377
Phatma2000 <fatma.kazimova17@mail.ru>
2024-01-11 17:11:27 +0400

    navbar/flo

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900 mb-5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
-            href="https://flowbite.com"
+           href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <Image
