fb2ae186bce945d6bc218dd02f0979e9402a5e73
NurjanHuseynova <nurcanhuseynova1@gmail.com>
2024-01-16 20:17:58 +0400

    improvements

--- app/components/mainPage.js
+++ app/components/mainPage.js
@@ -44,6 +44,8 @@ function MainPage() {
   const [tripData, setTripData] = useState([]);
   const [sendData, setSendData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [sendLoading, setSendLoading] = useState(false);
+
   const [activeButton, setActiveButton] = useState("forCarry");
   const [sendFormData, setSendFormData] = useState({
     sendCreateDate: "",
@@ -139,6 +141,7 @@ function MainPage() {
 
   const getSends = async (currentPage) => {
     try {
+      setSendLoading(true)
       let sendsData = await fetch(
         "http://carryforus-001-site1.htempurl.com/api/send/getsends ",
         {
@@ -178,7 +181,10 @@ function MainPage() {
 
         toast.error("Trip not found!");
       }
+      setSendLoading(false)
+
     } catch (error) {
+      setSendLoading(false)
       console.log("searchAds error: ", error);
     }
   };
@@ -360,7 +366,7 @@ function MainPage() {
                           >
                             {Object.entries(currency).map((v) => (
                               <>
-                                <option style={{ border: "none" }} value={v[1]}>
+                                <option  value={v[1]} >
                                   {v[0]}
                                 </option>
                               </>
@@ -689,8 +695,11 @@ function MainPage() {
                   <>
                     <div className="bg-white border box 2xl:mb-6  xl:mb-0 lg:mb-0 relative pt-4 pb-8 border-[#A0CCFF] border-solid rounded-[1rem]  h-[12rem] max-h-[16rem] hover:bg-[#449aff29] hover:border-[#0E6FE1] hover:shadow-xl transition duration-700 ease-in-out">
                       <div className="flex px-4 justify-between ">
-                        <div className="capitalize text-[#2F8EFF] font-semibold ">
-                          {v?.tripPlaceDetails[0]?.fromPlace}
+                      <div className="capitalize text-[#2F8EFF] font-semibold">
+                        {v?.tripPlaceDetails[0]?.fromPlace.length > 4 ?   
+                        <SecondTooltip text={v?.tripPlaceDetails[0]?.fromPlace}>
+                          { v?.tripPlaceDetails[0]?.fromPlace.slice(0,4) + "..." }
+                        </SecondTooltip> : v?.tripPlaceDetails[0]?.fromPlace }
                         </div>
                         <div
                           className="w-44 "
@@ -763,14 +772,16 @@ function MainPage() {
                             </>
                           ) : null}
                         </div>
+                        <div className="capitalize text-[#2F8EFF] font-semibold">
+                        {v?.tripPlaceDetails[0]?.toPlace.length > 4 ?   
                         <SecondTooltip text={v?.tripPlaceDetails[0]?.toPlace}>
-                          <div className="capitalize text-[#2F8EFF] font-semibold">
-                            {v?.tripPlaceDetails[0]?.toPlace.slice(0, 1) +
-                              "..."}
-                          </div>
-                        </SecondTooltip>
+                          { v?.tripPlaceDetails[0]?.toPlace.slice(0,4) + "..." }
+                        </SecondTooltip> : v?.tripPlaceDetails[0]?.toPlace }
+                        </div>
+                        
+                      
 
-                        <div className="distanceBox">
+                        <div className="distanceBox ">
                           <div>
                             {v?.tripPlaceDetails.length > 1 && (
                               <Image
@@ -784,11 +795,11 @@ function MainPage() {
                             )}
                           </div>
 
-                          <div>
+                          <div >
                             {v?.tripPlaceDetails.length > 1 && (
-                              <div className="  py-2 px-6 max-w-[325px] border border-solid border-[#b532ff75] absolute left-[33%] w-[315px] top-[50px] z-10 rounded-xl bg-white distance">
+                              <div className=" distanceScroll py-2 px-6 max-w-[325px] border border-solid border-[#b532ff75] absolute left-[33%] w-[315px] top-[45px] z-10 rounded-xl bg-white distance">
                                 {v.tripPlaceDetails.length > 1 &&
-                                  v.tripPlaceDetails?.map((v) => (
+                                  v.tripPlaceDetails.slice(1).map((v) => (
                                     <>
                                       <div className="flex justify-between">
                                         <div className="text-[#6B6890] font-semibold">
@@ -1041,17 +1052,41 @@ function MainPage() {
               />
             </>
           )}
-
+{sendLoading && (
+            <div role="status " className="absolute left-[50%] top-[50%]">
+              <svg
+                aria-hidden="true"
+                class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span class="sr-only">Loading...</span>
+            </div>
+          )}
           {activeButton === "forSend" && (
             <>
               <div className="grid grid-cols-3 gap-4 pt-6 px-6 mt-5">
                 {sendData.map((v) => (
                   <>
-                    <div className="bg-white border box 2xl:mb-6  xl:mb-0 lg:mb-0 relative pt-4 pb-8 border-[#8E65E7] border-solid rounded-[1rem]  h-[12rem] max-h-[16rem]  hover:border-[#7F4BED] hover:shadow-xl transition duration-700 ease-in-out">
+                    <div className="bg-white border box 2xl:mb-6  xl:mb-0 lg:mb-0 relative pt-4 pb-8  hover:bg-[#a784f22e] border-[#8E65E7] border-solid rounded-[1rem]  h-[12rem] max-h-[16rem]  hover:border-[#7F4BED] hover:shadow-xl transition duration-700 ease-in-out">
                       <div className="flex px-4 ">
-                        <div className="capitalize text-[#8E65E7] font-semibold ">
-                          {v?.sendPlaceDetails[0]?.fromPlace}
+                      <div className="capitalize text-[#8E65E7] font-semibold">
+                        {v?.sendPlaceDetails[0]?.fromPlace.length > 4 ?   
+                        <SecondTooltip text={v?.sendPlaceDetails[0]?.fromPlace}>
+                          { v?.sendPlaceDetails[0]?.fromPlace.slice(0,4) + "..." }
+                        </SecondTooltip> : v?.sendPlaceDetails[0]?.fromPlace }
                         </div>
+                      
                         <div
                           className="w-44 "
                           style={{
@@ -1063,19 +1098,19 @@ function MainPage() {
                         >
                           <Image
                             src="/icons/shipping.png"
-                            className="car absolute top-[7px]"
+                            className="car absolute top-[11px]"
                             width={25}
                             height={10}
                             alt="distance"
                             priority={true}
                           />
                         </div>
+                        <div className="capitalize text-[#8E65E7] font-semibold">
+                        {v?.sendPlaceDetails[0]?.toPlace.length > 4 ?   
                         <SecondTooltip text={v?.sendPlaceDetails[0]?.toPlace}>
-                          <div className="capitalize text-[#8E65E7] font-semibold">
-                            {v?.sendPlaceDetails[0]?.toPlace.slice(0, 1) +
-                              "..."}
-                          </div>
-                        </SecondTooltip>
+                          { v?.sendPlaceDetails[0]?.toPlace.slice(0,4) + "..." }
+                        </SecondTooltip> : v?.sendPlaceDetails[0]?.toPlace }
+                        </div>
 
                         <div>
                           {v?.sendPlaceDetails.length > 1 && (
